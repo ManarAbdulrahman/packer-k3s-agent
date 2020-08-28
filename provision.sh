166b15e99dda28c10792bcbdac78598e164e7a90
Omar Bazaid <omar.vip3@gmail.com>
2020-08-28 20:42:02 +0300

    fix nc

--- provision.sh
+++ provision.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 sudo apt update -y && sleep 30 && sudo apt install -y curl vim jq git docker.io
+sudo apt install  -y netcat-traditional
 sudo apt install -y make
 sudo groupadd docker
 sudo usermod -aG docker ubuntu
 export K3S_HOST=controlplane.phi.com
-export K3S_TOKEN=$(nc $K3S_HOST 1234)
+export K3S_TOKEN=$(nc.traditional $K3S_HOST 1234)
 export K3S_URL=https://$K3S_HOST:6443
 curl -sfL https://get.k3s.io | sh -
 
