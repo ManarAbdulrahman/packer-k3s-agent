5ce61223013869b3e6c9e5ef49d1194a86784a0d
Omar Bazaid <omar.vip3@gmail.com>
2020-08-28 13:49:46 +0300

    add install essintial

--- provision.sh
+++ provision.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo apt update -y && sleep 30 && sudo apt install -y curl vim jq git make docker.io
+sudo apt update -y && sleep 30 && sudo apt install -y curl vim jq git docker.io
+sudo apt install -y build-essential
+sudo apt install -y make
 sudo usermod -aG docker ubuntu
 export K3S_HOST=controlplane.phi.com
 export K3S_TOKEN=$(cat /home/ubuntu/token)
