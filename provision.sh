62650e2a373d953af61b0903c8517e1e4aec34c2
Omar Bazaid <omar.vip3@gmail.com>
2020-08-30 16:43:51 +0300

    fix stuff

--- provision.sh
+++ provision.sh
@@ -3,17 +3,18 @@ sudo apt update -y && sudo apt install -y curl vim git
 curl -L get.docker.com | sh
 sudo apt install  -y netcat-traditional
 sudo apt install -y make
+curl -sfL httpsdd://get.k3s.io | sh - 
 sudo usermod -aG docker ubuntu
 sudo cat <<EOF >> /home/ubuntu/serverinfo.sh
 #!/bin/bash
 export K3S_HOST=controlplane.phi.com
-export K3S_TOKEN="\$(nc.traditional $K3S_HOST 12345)"
-export K3S_URL=https://$K3S_HOST:6443
+export K3S_TOKEN="\$(nc.traditional \$K3S_HOST 12345)"
+export K3S_URL=https://\$K3S_HOST:6443
 echo 'export K3S_HOST=controlplane.phi.com' >> ~/.bashrc
-echo 'export K3S_TOKEN="\$(nc.traditional $K3S_HOST 12345)"' >> ~/.bashrc
-echo 'export K3S_URL=https://$K3S_HOST:6443' >> ~/.bashrc
-sudo systemct stop k3s
-sudo k3s agent --server ${K3S_URL} --token ${K3S_TOKEN}
+echo 'export K3S_TOKEN="\$(nc.traditional \$K3S_HOST 12345)"' >> ~/.bashrc
+echo 'export K3S_URL=https://\$K3S_HOST:6443' >> ~/.bashrc
+sudo systemctl stop k3s
+sudo k3s agent --server \${K3S_URL} --token \${K3S_TOKEN}
 EOF
 sudo chmod a+x /home/ubuntu/serverinfo.sh
 sudo cat <<EOF >> /home/ubuntu/get_token.service
@@ -31,5 +32,3 @@ sudo mv /home/ubuntu/get_token.service /lib/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable get_token
 sudo systemctl start get_token
-curl -sfL https://get.k3s.io | sh -
-
