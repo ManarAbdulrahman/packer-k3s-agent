902aab3b541d37fe7797052a58790ead91e1b4ef
Omar Bazaid <omar.vip3@gmail.com>
2020-08-30 23:56:19 +0300

    comment service

--- provision.sh
+++ provision.sh
@@ -17,18 +17,18 @@ sudo systemctl stop k3s
 sudo k3s agent --server \${K3S_URL} --token \${K3S_TOKEN}
 EOF
 sudo chmod a+x /home/ubuntu/serverinfo.sh
-sudo cat <<EOF >> /home/ubuntu/get_token.service
-[Unit]
-Description=Example systemd service.
-
-[Service]
-Type=simple
-ExecStart=/home/ubuntu/serverinfo.sh
-
-[Install]
-WantedBy=multi-user.target
-EOF
-sudo mv /home/ubuntu/get_token.service /lib/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl enable get_token
-sudo systemctl start get_token
+#sudo cat <<EOF >> /home/ubuntu/get_token.service
+#[Unit]
+#Description=Example systemd service.
+#
+#[Service]
+#Type=simple
+#ExecStart=/home/ubuntu/serverinfo.sh
+#
+#[Install]
+#WantedBy=multi-user.target
+#EOF
+#sudo mv /home/ubuntu/get_token.service /lib/systemd/system/
+#sudo systemctl daemon-reload
+#sudo systemctl enable get_token
+#sudo systemctl start get_token
