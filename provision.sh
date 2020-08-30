e2379bc5101079e90d617a5be5eb4ea0813f3684
Omar Bazaid <omar.vip3@gmail.com>
2020-08-31 01:26:25 +0300

    comment everything

--- provision.sh
+++ provision.sh
@@ -5,32 +5,32 @@ sudo apt install  -y netcat-traditional
 sudo apt install -y make
 sudo usermod -aG docker ubuntu
 #curl -sfL https://get.k3s.io | sh -
-sudo cat <<EOF >> /home/ubuntu/serverinfo.sh
-#!/bin/bash
-export K3S_HOST=controlplane.phi.com
-export K3S_TOKEN="\$(nc.traditional \$K3S_HOST 12345)"
-export K3S_URL=https://\$K3S_HOST:6443
-echo 'export K3S_HOST=controlplane.phi.com' >> ~/.bashrc
-echo 'export K3S_TOKEN="\$(nc.traditional \$K3S_HOST 12345)"' >> ~/.bashrc
-echo 'export K3S_URL=https://\$K3S_HOST:6443' >> ~/.bashrc
-#sudo systemctl stop k3s
-#sudo k3s agent --server \${K3S_URL} --token \${K3S_TOKEN}
-curl -sfL https://get.k3s.io | sh -
-sudo systemctl stop get_token
-EOF
-sudo chmod a+x /home/ubuntu/serverinfo.sh
-sudo cat <<EOF >> /home/ubuntu/get_token.service
-[Unit]
-Description=Example systemd service.
+#sudo cat <<EOF >> /home/ubuntu/serverinfo.sh
+##!/bin/bash
+#export K3S_HOST=controlplane.phi.com
+#export K3S_TOKEN="\$(nc.traditional \$K3S_HOST 12345)"
+#export K3S_URL=https://\$K3S_HOST:6443
+#echo 'export K3S_HOST=controlplane.phi.com' >> ~/.bashrc
+#echo 'export K3S_TOKEN="\$(nc.traditional \$K3S_HOST 12345)"' >> ~/.bashrc
+#echo 'export K3S_URL=https://\$K3S_HOST:6443' >> ~/.bashrc
+##sudo systemctl stop k3s
+##sudo k3s agent --server \${K3S_URL} --token \${K3S_TOKEN}
+#curl -sfL https://get.k3s.io | sh -
+#sudo systemctl stop get_token
+#EOF
+#sudo chmod a+x /home/ubuntu/serverinfo.sh
+#sudo cat <<EOF >> /home/ubuntu/get_token.service
+#[Unit]
+#Description=Example systemd service.
 
-[Service]
-Type=simple
-ExecStart=/home/ubuntu/serverinfo.sh
+#[Service]
+#Type=simple
+#ExecStart=/home/ubuntu/serverinfo.sh
 
-[Install]
-WantedBy=multi-user.target
-EOF
-sudo mv /home/ubuntu/get_token.service /lib/systemd/system/
-sudo systemctl daemon-reload
+#[Install]
+#WantedBy=multi-user.target
+#EOF
+#sudo mv /home/ubuntu/get_token.service /lib/systemd/system/
+#sudo systemctl daemon-reload
 #sudo systemctl enable get_token
 #sudo systemctl start get_token
